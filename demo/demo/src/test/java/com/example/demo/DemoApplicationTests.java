712f37c9fa1d20dd8351aff3c6893db17c5ce226
cukelli <anastasija.cukelj@gmail.com>
2024-08-06 11:27:20 +0200

    fix

--- demo/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,8 +2,11 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = DemoApplication.class)
+
 class DemoApplicationTests {
 
 	@Test
